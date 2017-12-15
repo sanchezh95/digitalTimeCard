2dac659fd38ff5170973fde7c85f1533d74426a4
Huimee Sanchez <sanchezh95@gmail.com>
2017-12-15 14:58:32 -0800

    get times of clock-in and out

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,31 +9,60 @@ export class MainComponent implements OnInit {
 
   working = false;
   off = false;
+  on = false;
   clockInTime;
   clockOutTime;
   hoursWorked;
   month;
   day;
-  time;
-  hour;
-  minutes;
 
   constructor() {
     let date = new Date();
     this.month = date.toLocaleString('en-us', {month: 'long'});
     this.day = date.getDate();
-    this.hour = date.getHours();
-    this.minutes = date.getMinutes();
-    this.time = this.hour + ':' + this.minutes;
   }
 
   clockIn() {
     this.working = true;
+    this.on = true;
+    this.clockInTime = this.getTime();
   }
 
   clockOut() {
     this.working = false;
     this.off = true;
+    this.clockOutTime = this.getTime();
+
+    // Calculate total hours worked
+    // For 'regular' work hours - will not calculate > 12 hours of work
+    let inTime = this.clockInTime.split(':');
+    let outTime = this.clockOutTime.split(':');
+
+    let inHr = parseInt(inTime[0]);
+    let outHr = parseInt(outTime[0]);
+    let inMin = parseInt(inTime[1]) / 60;
+    let outMin = parseInt(outTime[1]) / 60;
+
+    let clockIn = inHr + inMin;
+    let clockOut = outHr + outMin;
+
+    this.hoursWorked = clockOut - clockIn;
+    this.hoursWorked = this.hoursWorked.toFixed(2);
+  }
+
+  getTime() {
+    let date = new Date();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+
+    // Format of time - HH:MM
+    if (minutes < 10) {
+      let min = '0' + minutes;
+      return (hour + ':' + min);
+    }
+    else {
+      return (hour + ':' + minutes);
+    }
   }
 
   ngOnInit() {}
